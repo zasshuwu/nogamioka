f75ebbcde87108d4b1d21b82b8d1986a837d09eb
HA <hoanganh.theodore@icloud.com>
2020-07-20 13:52:56 -0400

    fixed project table

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,73 +1,88 @@
-import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
-import './Projects.scss';
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "./Projects.scss";
 
 const Projects = () => (
-	<Container id="projects" className="projects card">
-		<h2>Projects</h2>
-		<br />
-
-		<Row style={{ fontWeight: 'bold' }}>
-			<Col xs="auto">#</Col>
-			<Col xs={3}>ID</Col>
-			<Col>Position</Col>
-			<Col>Description</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">1</Col>
-			<Col xs={3}>
-				<a href="https://github.com/zasshuwu/duplicated_accelcamp.git">Inertia Sensing Lab</a>
-			</Col>
-			<Col>Research Student, Software Architect</Col>
-			<Col>Apply machine learning on accelerometric data processing using Python and Tensorflow.</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">2</Col>
-			<Col xs={3}>FaceCookery (WIP)</Col>
-			<Col>Front-end Developer</Col>
-			<Col>A feature-rich recipe-sharing website using React, Bootstrap, Django.</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">3</Col>
-			<Col xs={3}>
-				<a href="http://maiboatservice.com">Mai Boat Service</a>
-			</Col>
-			<Col>Full-stack Web Developer and SEO</Col>
-			<Col>Family-owned boat repair enterprise based in Orlando, Florida. Static webhosting.</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">4</Col>
-			<Col xs={3}>
-				<a href="http://github.com/zasshuwu/arduino-phys4eng-w2020.git">Maze navigating vehicle</a>
-			</Col>
-			<Col>Arduino programmer (C++)</Col>
-			<Col>College engineering project. 3D-printed vehicle navigates by ultrasonic sensors controlled by an Arduino Uno.</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">5</Col>
-			<Col xs={3}>
-				<a href="http://github.com/zasshuwu/mrca-sim-r.git">R project in Statistics</a>
-			</Col>
-			<Col>n/A</Col>
-			<Col>College project using R in statistical analysis. Simulate Most Recent Common Ancestor (MRCA).</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">6</Col>
-			<Col xs={3}>
-				<a href="https://blog.nogamioka.com">Personal blog</a>
-			</Col>
-			<Col>n/A</Col>
-			<Col>Bilingual blog about aspects of my life. Built with Jekyll blog framework. Hosted on Github Pages.</Col>
-		</Row>
-		<Row>
-			<Col xs="auto">7</Col>
-			<Col xs={3}>
-				<a href="https://github.com/zasshuwu/researchpapers">Research papers</a>
-			</Col>
-			<Col>n/A</Col>
-			<Col>My (co-)authored research papers.</Col>
-		</Row>
-	</Container>
+    <Container id="projects" className="projects card">
+        <h2>Projects</h2>
+        <br />
+        <div>
+            <Row style={{ fontWeight: "bold" }}>
+                <Col xs={1}>#</Col>
+                <Col>ID</Col>
+                <Col>Position</Col>
+                <Col xs={7}>Description</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>1</Col>
+                <Col>
+                    <a href="https://github.com/zasshuwu/duplicated_accelcamp.git">Inertia Sensing Lab</a>
+                </Col>
+                <Col>Research Student, Software Architect</Col>
+                <Col xs={7}>Apply machine learning on accelerometric data processing using Python and Tensorflow.</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>2</Col>
+                <Col>FaceCookery (WIP)</Col>
+                <Col>Front-end Developer</Col>
+                <Col xs={7}>A feature-rich recipe-sharing website using React, Bootstrap, Django.</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>3</Col>
+                <Col>
+                    <a href="http://maiboatservice.com">Mai Boat Service</a>
+                </Col>
+                <Col>Full-stack Web Developer and SEO</Col>
+                <Col xs={7}>Family-owned boat repair enterprise based in Orlando, Florida. Static webhosting.</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>4</Col>
+                <Col>
+                    <a href="http://github.com/zasshuwu/arduino-phys4eng-w2020.git">Maze navigating vehicle</a>
+                </Col>
+                <Col>Arduino programmer (C++)</Col>
+                <Col xs={7}>College engineering project. 3D-printed vehicle navigates by ultrasonic sensors controlled by an Arduino Uno.</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>5</Col>
+                <Col>
+                    <a href="http://github.com/zasshuwu/mrca-sim-r.git">R project in Statistics</a>
+                </Col>
+                <Col>n/A</Col>
+                <Col xs={7}>College project using R in statistical analysis. Simulate Most Recent Common Ancestor (MRCA).</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>6</Col>
+                <Col>
+                    <a href="https://blog.nogamioka.com">Personal blog</a>
+                </Col>
+                <Col>n/A</Col>
+                <Col xs={7}>Bilingual blog about aspects of my life. Built with Jekyll blog framework. Hosted on Github Pages.</Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col xs={1}>7</Col>
+                <Col>
+                    <a href="https://github.com/zasshuwu/researchpapers">Research papers</a>
+                </Col>
+                <Col>n/A</Col>
+                <Col xs={7}>My (co-)authored research papers.</Col>
+            </Row>
+        </div>
+    </Container>
 );
 
 export default Projects;
