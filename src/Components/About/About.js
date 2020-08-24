b9fd4b4173ba04456f047f810ee027897a6bed11
HA <hoanganh.theodore@icloud.com>
2020-08-24 16:10:41 -0400

    added gh stats lang

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -41,6 +41,14 @@ const About = () => (
 			<li>Machine Learning: Tensorflow (Python + JS)</li>
 			<li>Server: Apache2, Nginx, node, vanillaJS</li>
 		</ul>
+		<div className="gh-stats-wrapper">
+			<img
+				style={{
+					filter: "none",
+				}}
+				src="https://camo.githubusercontent.com/02aa33baf0ebed4c72f4720263427b658ac1762d/68747470733a2f2f6769746875622d726561646d652d73746174732e7a617373687577752e76657263656c2e6170702f6170692f746f702d6c616e67732f3f757365726e616d653d7a61737368757775266c61796f75743d636f6d70616374267468656d653d746f6b796f6e6967687426686964655f626f726465723d74727565"
+			></img>
+		</div>
 		<br />
 		<h3>Language Proficiency</h3>
 		<p>Vietnamese &gt; English &gt; French &gt; Japanese, German.</p>
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -26,7 +26,9 @@ const Intro = () => (
 					<br />
 					<div className="gh-stats-wrapper">
 						<img
-							style={{ boxShadow: "10px 10px 14px -9px rgba(200, 200, 200, 1)" }}
+							style={{
+								boxShadow: "10px 10px 14px -9px rgba(200, 200, 200, 1)",
+							}}
 							src="https://camo.githubusercontent.com/4e5a548f866498d0775b7db48514c2bc855c5a00/68747470733a2f2f6769746875622d726561646d652d73746174732e7a617373687577752e76657263656c2e6170702f6170693f757365726e616d653d7a617373687577752673686f775f69636f6e733d7472756526686964655f626f726465723d74727565"
 						></img>
 					</div>
