c30eb5830675a3c52b8bda8c75e0419e5b4c7c79
HA <hoanganh.theodore@icloud.com>
2020-08-24 16:20:46 -0400

    wot?

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => (
 				<Nav.Link href="#projects">Projects</Nav.Link>
 			</Col>
 			<Col>
-				<Nav.Link href="#about">Abouts</Nav.Link>
+				<Nav.Link href="#about">About</Nav.Link>
 			</Col>
 			{/* <Col>
 				<Nav.Link href="#contact">Contact</Nav.Link>
