98031faee2ef306134646e11189876406fda44ef
HA <iam@hoanganh.tech>
2021-07-20 13:46:19 -0400

    continued

--- components/terminal.js
+++ components/terminal.js
@@ -4,13 +4,18 @@ import { Terminal } from 'xterm'
 function TerminalComponent() {
     useEffect(() => {
         var term = new Terminal();
+        var num_char = 0;
         term.open(document.getElementById("terminal"));
         term.write("aanh@portfolio \x1B[1;3;31m$\x1B[0m ");
         term.onData(
             e => {
-                if (e.charCodeAt(0) == 127)
+                if (e.charCodeAt(0) == 127 && num_char >= 1) {
+                    console.log(num_char);
                     term.write("\b \b");
-                term.write(e)
+                    num_char = num_char - 1;
+                }
+                term.write(e);
+                num_char = num_char + 1;
             }
         )
     }, []);
