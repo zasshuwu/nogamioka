93c184612d66f3e1ea88ca886dd12caaddabca1a
Anh Nguyen <hanguyen@genetec.com>
2021-09-08 12:48:53 -0400

    added linux maintenance page

---
+++ pages/maintenance/linux.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function linuxMaintenance() {
+    return (
+        <div style={{display: "flex", width: "100vw", height: "100vh", alignItems: "center", justifyContent: "center"}} className="linux_maintenance">
+            <div>
+                <h1>The service you are looking at is currently down for scheduled maintenance.</h1>
+            </div>
+        </div>
+    )
+}
