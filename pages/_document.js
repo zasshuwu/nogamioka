e4b0719597a6fb0ea406df76f0305ee8eb863085
Anh <iam@hoanganh.tech>
2022-01-28 20:19:56 -0500

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,22 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,300;0,700;1,400&display=swap"
 						rel="stylesheet"
 					/>
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+									window.dataLayer = window.dataLayer || [];
+									function gtag(){dataLayer.push(arguments);}
+									gtag('js', new Date());
+									gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+									page_path: window.location.pathname,
+									});
+								`,
+						}}
+					/>
 				</Head>
 				<body style={{ backgroundColor: "#1a1a1a" }}>
 					<Main />
