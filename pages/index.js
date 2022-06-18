af0d57e644fcf926082d8b449d030172a3fd9d8f
Anh Hoang Nguyen <iam@hoanganh.tech>
2022-06-18 00:00:40 -0400

    change glow colors

--- pages/index.js
+++ pages/index.js
@@ -9,121 +9,130 @@ import Typed from "typed.js";
 import useSWR from "swr";
 
 const TypedBios = () => {
-	const el = useRef(null);
-	const typed = useRef(null);
+  const el = useRef(null);
+  const typed = useRef(null);
 
-	useEffect(() => {
-		const options = {
-			backDelay: 800,
-			backSpeed: 5,
-			loop: true,
-			stringsElement: "#bios",
-			typeSpeed: 25,
-		};
-		typed.current = new Typed(el.current, options);
-		return () => typed.current.destroy();
-	}, []);
+  useEffect(() => {
+    const options = {
+      backDelay: 800,
+      backSpeed: 5,
+      loop: true,
+      stringsElement: "#bios",
+      typeSpeed: 25,
+    };
+    typed.current = new Typed(el.current, options);
+    return () => typed.current.destroy();
+  }, []);
 
-	return (
-		<div>
-			<ul id="bios" className="hidden">
-				<li>
-					You can simply call me <b className="font-medium">Anh</b>.
-				</li>
-				<li className="word-wrap">I can communicate in VN, EN, FR, JP, DE.</li>
-				<li>I love listening to music.</li>
-				<li>I play guitar.</li>
-				<li>A lover for good food.</li>
-				<li>A team-player and result-oriented person.</li>
-				<li>Capable of administrating Linux systems</li>
-				<li>...and Windows Server.</li>
-				<li>Good knowledge on networking protocols.</li>
-				<li>Well-versed in Docker, k8s, and IaC tools.</li>
-				<li>Firm grasp on security concepts &amp; frameworks.</li>
-			</ul>
-			<span ref={el} className="text-neutral-900 dark:text-neutral-200" />
-		</div>
-	);
+  return (
+    <div>
+      <ul id="bios" className="hidden">
+        <li>
+          You can simply call me <b className="font-medium">Anh</b>.
+        </li>
+        <li className="word-wrap">
+          I can communicate in VN, EN, FR, JP, DE.
+        </li>
+        <li>I love listening to music.</li>
+        <li>I play guitar.</li>
+        <li>A lover for good food.</li>
+        <li>A team-player and result-oriented person.</li>
+        <li>Capable of administrating Linux systems</li>
+        <li>...and Windows Server.</li>
+        <li>Good knowledge on networking protocols.</li>
+        <li>Well-versed in Docker, k8s, and IaC tools.</li>
+        <li>Firm grasp on security concepts &amp; frameworks.</li>
+      </ul>
+      <span
+        ref={el}
+        className="text-neutral-900 dark:text-neutral-200"
+      />
+    </div>
+  );
 };
 
 export default function Splash() {
-	let d = new Date();
+  let d = new Date();
 
-	const fetcher = (url) => fetch(url).then((res) => res.json());
-	const { data, error } = useSWR("/api/spotify", fetcher);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR("/api/spotify", fetcher);
 
-	const [themeBtnTxt, setThemeBtnTxt] = useState("dark");
-	// useEffect(() => {
-	// 	setThemeBtnTxt(localStorage.getItem("theme"));
-	// }, []);
+  const [themeBtnTxt, setThemeBtnTxt] = useState("dark");
+  // useEffect(() => {
+  // 	setThemeBtnTxt(localStorage.getItem("theme"));
+  // }, []);
 
-	return (
-		<div className="overflow-hidden font-['Fira_Sans'] w-screen sm:h-screen h-[88vh] dark:text-white bg-white dark:bg-neutral-900 flex flex-col justify-center items-center transition-all ease-in-out">
-			<SEO title="aaanh 👾"></SEO>
-			<div className="relative sm:skew-y-[2deg] p-8 flex flex-col justify-center items-center min-h-[80vh] sm:border sm:min-h-[70vh] rounded-md sm:shadow-lg dark:shadow-rose-500/50 sm:shadow-purple-500/50 dark:border-neutral-600 border-neutral-200">
-				<div className="sm:block relative -top-4 w-16 h-6 border border-neutral-600 rounded-xl hidden"></div>
-				<div className="rounded-full h-32 w-32 sm:h-48 sm:w-48 shadow-2xl my-4 relative sm:-top-2 -top-4">
-					<Image
-						height={"200px"}
-						layout="responsive"
-						src="/splash-photo.jpeg"
-						style={{ borderRadius: "50%" }}
-						width={"200px"}
-					></Image>
-				</div>
-				<div className="text-center">
-					<h1 className="font-bold">Anh Hoang Nguyen</h1>
-					<h2 className="">
-						Software Engineer{" "}
-						{
-							<span className="sm:hidden">
-								<br></br>
-							</span>
-						}{" "}
-						DevOps &amp; Web Development
-					</h2>
-					<h3 className="text-neutral-500">Bachelor of Computer Engineering &apos;24</h3>
-					<TypedBios></TypedBios>
-					<div className="my-2 font-mono">
-						<Link href="/home">goto: full site</Link>
-					</div>
-					<div className="flex space-x-4 justify-center my-4">
-						<a href="https://linkedin.com/in/aaanh">
-							<SiLinkedin size={28}></SiLinkedin>
-						</a>
-						<a href="https://github.com/aaanh">
-							<SiGithub size={28}></SiGithub>
-						</a>
-						<a href="mailto:iam@hoanganh.dev">
-							<IoMdMail size={28}></IoMdMail>
-						</a>
-						<a href="https://twitter.com/yeetusmagus">
-							<SiTwitter size={28}></SiTwitter>
-						</a>
-					</div>
-				</div>
-				<div>
-					<button
-						className="hover:bg-violet-500 hover:text-white dark:text-neutral-300 hover:dark:text-neutral-900 p-1 px-4 rounded-lg transition-all ease-in-out dark:hover:bg-rose-400 border dark:border-rose-400 dark:border-opacity-40 border-violet-500 border-opacity-40"
-						onClick={() => {
-							if (localStorage.getItem("theme") === "dark") {
-								document.documentElement.classList.remove("dark");
-								localStorage.setItem("theme", "light");
-								setThemeBtnTxt("light");
-							} else {
-								document.documentElement.classList.add("dark");
-								localStorage.setItem("theme", "dark");
-								setThemeBtnTxt("dark");
-							}
-						}}
-					>
-						{themeBtnTxt === "dark" ? "praise the sun" : "race into the night"}
-					</button>
-				</div>
-				<div className="mt-4">
-					<SpotifyBadge data={data}></SpotifyBadge>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="overflow-hidden font-['Fira_Sans'] w-screen sm:h-screen h-[88vh] dark:text-white bg-white dark:bg-neutral-900 flex flex-col justify-center items-center transition-all ease-in-out">
+      <SEO title="aaanh 👾"></SEO>
+      <div className="relative sm:skew-y-[2deg] p-8 flex flex-col justify-center items-center min-h-[80vh] sm:border sm:min-h-[70vh] rounded-md sm:shadow-lg dark:shadow-cyan-500/50 sm:shadow-purple-500/50 dark:border-neutral-600 border-neutral-200">
+        <div className="sm:block relative -top-4 w-16 h-6 border border-neutral-600 rounded-xl hidden"></div>
+        <div className="rounded-full h-32 w-32 sm:h-48 sm:w-48 shadow-2xl my-4 relative sm:-top-2 -top-4">
+          <Image
+            height={"200px"}
+            layout="responsive"
+            src="/splash-photo.jpeg"
+            style={{ borderRadius: "50%" }}
+            width={"200px"}
+          ></Image>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">Anh Hoang Nguyen</h1>
+          <h2 className="">
+            Software Engineer{" "}
+            {
+              <span className="sm:hidden">
+                <br></br>
+              </span>
+            }{" "}
+            DevOps &amp; Web Development
+          </h2>
+          <h3 className="text-neutral-500">
+            Bachelor of Computer Engineering &apos;24
+          </h3>
+          <TypedBios></TypedBios>
+          <div className="my-2 font-mono">
+            <Link href="/home">goto: full site</Link>
+          </div>
+          <div className="flex space-x-4 justify-center my-4">
+            <a href="https://linkedin.com/in/aaanh">
+              <SiLinkedin size={28}></SiLinkedin>
+            </a>
+            <a href="https://github.com/aaanh">
+              <SiGithub size={28}></SiGithub>
+            </a>
+            <a href="mailto:iam@hoanganh.dev">
+              <IoMdMail size={28}></IoMdMail>
+            </a>
+            <a href="https://twitter.com/yeetusmagus">
+              <SiTwitter size={28}></SiTwitter>
+            </a>
+          </div>
+        </div>
+        <div>
+          <button
+            className="hover:bg-violet-500 hover:text-white dark:text-neutral-300 hover:dark:text-neutral-900 p-1 px-4 rounded-lg transition-all ease-in-out dark:hover:bg-cyan-400 border dark:border-cyan-400 dark:border-opacity-40 border-violet-500 border-opacity-40"
+            onClick={() => {
+              if (localStorage.getItem("theme") === "dark") {
+                document.documentElement.classList.remove("dark");
+                localStorage.setItem("theme", "light");
+                setThemeBtnTxt("light");
+              } else {
+                document.documentElement.classList.add("dark");
+                localStorage.setItem("theme", "dark");
+                setThemeBtnTxt("dark");
+              }
+            }}
+          >
+            {themeBtnTxt === "dark"
+              ? "praise the sun"
+              : "race into the night"}
+          </button>
+        </div>
+        <div className="mt-4">
+          <SpotifyBadge data={data}></SpotifyBadge>
+        </div>
+      </div>
+    </div>
+  );
 }
