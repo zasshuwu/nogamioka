66c95d92bdb91548c3035f0f64a88e8cd73455e1
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-06-25 15:53:35 -0400

    working on mongodb integration

---
+++ components/test/Result.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+const Result = ({ results }) => {
+  let [bg, setBg] = useState("");
+
+  useEffect(() => {
+    setBackground();
+  });
+
+  const setBackground = () => {
+    let min = results.target - results.variant;
+    let max = results.target + results.variant;
+
+    if (results.total >= min && results.total <= max) {
+      setBg("bg-green-500");
+    } else if (results.total < min) {
+      setBg("bg-blue-500");
+    } else {
+      setBg("bg-red-500");
+    }
+  };
+
+  return (
+    <div className={bg + " w-1/4 p-4 text-white"}>
+      <h2 className="text-3xl font-bold">
+        {results.total}
+        <div className="flex text-sm p-4">
+          <div className="w-1/3">
+            {results.target - results.variant}
+          </div>
+          <div className="w-1/3 font-bold">{results.target}</div>
+          <div className="w-1/3">
+            {results.target + results.variant}
+          </div>
+        </div>
+      </h2>
+      <h3 className="text-xl">{results.label}</h3>
+    </div>
+  );
+};
+
+export default Result;
