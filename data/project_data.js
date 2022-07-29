9d0ef470e0fd7cc1976e9b9f4e644a0a60076e73
Anh Hoang Nguyen <aaanh@users.noreply.github.com>
2022-07-29 16:01:30 -0400

    rm shit

--- data/project_data.js
+++ data/project_data.js
@@ -59,15 +59,15 @@ export const projectData = [
     image: "/coen244/coen244-demo.gif",
     link_ref: "https://github.com/aaanh/meaningful-graph-traversal",
   }),
-  createProject({
-    title: "Imaginary Hackathon Website",
-    description: [
-      "Originally developed for ConUHacks VI, but project was scrapped in the middle of development. It features CTF-style games. For sentimental values, I finished it up and deployed in halcyon.",
-    ],
-    stack: "Nextjs, Material-UI",
-    iframe: "https://halcyon.hoanganh.dev",
-    link_ref: "https://halcyon.hoanganh.dev",
-  }),
+  // createProject({
+  //   title: "Imaginary Hackathon Website",
+  //   description: [
+  //     "Originally developed for ConUHacks VI, but project was scrapped in the middle of development. It features CTF-style games. For sentimental values, I finished it up and deployed in halcyon.",
+  //   ],
+  //   stack: "Nextjs, Material-UI",
+  //   iframe: "https://halcyon.hoanganh.dev",
+  //   link_ref: "https://halcyon.hoanganh.dev",
+  // }),
 
   createProject({
     title: "Maze-Navigating Vehicle",
