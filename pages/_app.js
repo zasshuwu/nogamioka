331c8a9da292b9c25f57e1735e881872be04fe4a
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-11-07 19:41:17 -0500

    +logrocket

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import LogRocket from 'logrocket';
+
+LogRocket.init('6m72da/homepage');
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
