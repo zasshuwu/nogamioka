81257d53eb85b2fc0f07f3065f9c96fa2fa65cf0
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-11-20 15:11:51 -0500

    try out vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
-import '../styles/globals.css'
-import LogRocket from 'logrocket';
+import "../styles/globals.css";
+import LogRocket from "logrocket";
+import { Analytics } from "@vercel/analytics/react";
 
-LogRocket.init('6m72da/homepage');
-
+LogRocket.init("6m72da/homepage");
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
