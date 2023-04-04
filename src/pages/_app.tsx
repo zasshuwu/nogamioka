90ecfb29e0e9472e2a0638d9a8be5bca9b56bcee
Anh H Nguyen <iam@hoanganh.dev>
2023-04-04 12:40:00 -0400

    Update app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import LogRocket from "logrocket";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import LogRocket from "logrocket";
 
-LogRocket.init(process.env.LOGROCKET_MONITORING || "");
+LogRocket.init("6m72da/homepage");
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
