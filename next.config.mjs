5d349748bc83010544c34f8b69c20973108e2b5a
Anh Hoang Nguyen <anhnguyen@aaanh.com>
2023-11-03 13:04:59 -0400

    Refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,8 @@
-// @ts-check
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  // modularizeImports: {
-  //   'kbar': {
-  //     transform: 'kbar/{{member}}'
-  //   }
-  // },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
