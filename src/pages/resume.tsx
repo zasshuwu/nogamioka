daa5e478af063f44929675a61167c151c08a12d3
Anh H Nguyen <AnhNguyen@aaanh.com>
2024-04-01 22:50:43 -0400

    Update: Improve Resume UX

---
+++ src/pages/resume.tsx
@@ -0,0 +1,9 @@
+"use client";
+import RootLayout from "~/layouts/IndexLayout";
+import { Document } from "react-pdf";
+
+export default function Resume() {
+  return (
+    <RootLayout>{/* <Document file="anh-resume.pdf"></Document> */}</RootLayout>
+  );
+}
