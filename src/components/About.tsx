bdcde3179acb1f3f84ef1ea708ebd2f35127e908
Anh H Nguyen <iam@hoanganh.dev>
2024-06-21 03:42:07 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,9 @@
 const About = () => {
   return <div className="prose dark:prose-invert p-8">
-    <p>This portfolio is built with 🩸 and 😭.</p>
-    <p>I bootstrapped it with <code>npx create-t3-app</code>. Styled with Tailwind, no theme or component libraries were used, unlike the previous incarnation of this portfolio (DaisyUI).</p>
-    <p>The site is hosted on Vercel and its source code is on <a href="https://github.com/aaanh/homepage">Github</a></p>
-    <p>As you can tell, I shamelessly skadooshed the styles from macOS.</p>
+    <p>Hi, my name is *chicka chicka* Sli-.... Anh, welcome to my portfolio.</p>
+    <p>The current iteration of the project was bootstrapped with <code>npx create-next-app</code>, running the bleeding edge Next.js 15 and React 19. Styled with Tailwind.</p>
+    <p>The site is hosted on Vercel and its source code is available on <a href="https://github.com/aaanh/homepage">Github</a>.</p>
+    <p>As you can tell, I shamelessly skadooshed the look and feel from Visual Studio Code.</p>
   </div>
 }
 
