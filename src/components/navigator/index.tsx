bd344635547a7a183239f29bf4f5b2944a5fbf7e
Anh Hoang Nguyen <anhnguyen@aaanh.com>
2024-08-25 16:31:29 -0400

    good progress

---
+++ src/components/navigator/index.tsx
@@ -0,0 +1,35 @@
+import { FaLink } from "react-icons/fa";
+import Container from "../container";
+import ContentCard from "../content-card";
+import Link from "next/link";
+import { LucideCompass, LucideLink2 } from "lucide-react";
+
+export default function Navigator() {
+  const targets = ["Experiences", "Projects", "Resume", "Contact", "Gears"];
+
+  return (
+    <Container className="lg:grid-cols-[2fr_3fr]">
+      <ContentCard className="text-4xl text-accent border-none">
+        <LucideCompass />
+        &nbsp; Navigator
+      </ContentCard>
+      <Container className="grid-cols-2">
+        {targets.map((target) => (
+          <Link
+            key={target}
+            className="group"
+            href={`#${target.toLowerCase()}`}
+          >
+            <ContentCard className="text-3xl relative text-left justify-start">
+              <span className="relative group-hover:text-green-400">
+                {target}
+              </span>
+              &nbsp;
+              <LucideLink2 className="opacity-0 group-hover:opacity-100 transition-all ease-in-out" />
+            </ContentCard>
+          </Link>
+        ))}
+      </Container>
+    </Container>
+  );
+}
